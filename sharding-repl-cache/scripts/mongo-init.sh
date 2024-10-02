c3f8f343eace298d4c2a176da20be754677bc7c3
Alekseev Andrey <aialekseev@nota.tech>
2024-10-03 00:13:50 +0300

    Task 4

--- sharding-repl-cache/scripts/mongo-init.sh
+++ sharding-repl-cache/scripts/mongo-init.sh
@@ -98,3 +98,15 @@ use somedb;
 db.helloDoc.countDocuments();
 
 EOF
+
+###
+# Инициализируем кластер Redis
+###
+
+docker compose exec -T redis-1 <<EOF
+
+echo "yes" | redis-cli --cluster create redis-1:6379 redis-2:6379 redis-3:6379 redis-4:6379 redis-5:6379 redis-6:6379 --cluster-replicas 1
+
+redis-cli cluster nodes
+
+EOF
