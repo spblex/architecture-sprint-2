54c9dc1dc030d4897807c212d7f330020bc6d56f
Alekseev Andrey <aialekseev@nota.tech>
2024-10-02 03:12:18 +0300

    Task 2

--- mongo-sharding/scripts/mongo-init.sh
+++ mongo-sharding/scripts/mongo-init.sh
@@ -1,11 +1,96 @@
 #!/bin/bash
 
 ###
-# Инициализируем бд
+# Инициализируем MongoDB Config Server
 ###
 
-docker compose exec -T mongodb1 mongosh <<EOF
-use somedb
-for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
+docker compose exec -T mongo-config-server mongosh --port 27017 --quiet <<EOF
+rs.initiate(
+  {
+    _id : "mongo-config-server",
+       configsvr: true,
+    members: [
+      { _id : 0, host : "mongo-config-server:27017" }
+    ]
+  }
+);
 EOF
 
+###
+# Инициализируем MongoDB Shard 1
+###
+
+docker compose exec -T mongo-shard-1 mongosh --port 27019 --quiet <<EOF
+rs.initiate(
+  {
+    _id : "mongo-shard-1",
+    members: [
+      { _id : 0, host : "mongo-shard-1:27019" }
+    ]
+  }
+);
+EOF
+
+###
+# Инициализируем MongoDB Shard 2
+###
+
+docker compose exec -T mongo-shard-2 mongosh --port 27020 --quiet <<EOF
+rs.initiate(
+  {
+    _id : "mongo-shard-2",
+    members: [
+      { _id : 1, host : "mongo-shard-2:27020" }
+    ]
+  }
+);
+EOF
+
+###
+# Инициализируем MongoDB Router
+###
+
+docker compose exec -T mongo-router mongosh --port 27018 --quiet <<EOF
+
+sh.addShard( "mongo-shard-1/mongo-shard-1:27019");
+sh.addShard( "mongo-shard-2/mongo-shard-2:27020");
+
+sh.enableSharding("somedb");
+sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
+
+EOF
+
+###
+# Генерируем тестовые данные в MongoDB
+###
+
+docker compose exec -T mongo-router mongosh --port 27018 --quiet <<EOF
+
+use somedb;
+
+for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"doc-"+i});
+db.helloDoc.countDocuments();
+
+EOF
+
+###
+# Проверяем документы на MongoDB Shard 1
+###
+
+docker compose exec -T mongo-shard-1 mongosh --port 27019 --quiet <<EOF
+
+use somedb;
+db.helloDoc.countDocuments();
+
+EOF
+
+###
+# Проверяем документы на MongoDB Shard 2
+###
+
+docker compose exec -T mongo-shard-1 mongosh --port 27020 --quiet <<EOF
+
+use somedb;
+db.helloDoc.countDocuments();
+
+EOF
