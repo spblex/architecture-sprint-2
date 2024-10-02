2c19de018ea13197c0af6ca893f2188a9d11afb5
Alekseev Andrey <aialekseev@nota.tech>
2024-10-03 00:25:23 +0300

    Task 3

--- mongo-sharding-repl/scripts/mongo-init.sh
+++ mongo-sharding-repl/scripts/mongo-init.sh
@@ -72,7 +72,7 @@ docker compose exec -T mongo-router mongosh --port 27018 --quiet <<EOF
 
 use somedb;
 
-for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"doc-"+i});
+for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i});
 db.helloDoc.countDocuments();
 
 EOF
